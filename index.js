b2429a332849eede4cdc03f5247b6b28043697ec
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-01-16 01:09:48 +0200

    add new package, connect to mongo atlas (in browser)

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const homeRoutes = require('./routes/home')
-const cardRoutes = require('./routes/card')
-const addRoutes = require('./routes/add')
-const coursesRoutes = require('./routes/courses')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/card', cardRoutes)
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+const url = `mongodb+srv://dennypearce:aFJaYKkU7xtIDlpl@cluster0.odpci.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
