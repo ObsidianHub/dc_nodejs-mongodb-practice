5ecad2f081e70f48cd6a22ad063a8be905ff2660
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-01-17 00:21:27 +0200

    user model

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
-const homeRoutes = require('./routes/home')
-const cardRoutes = require('./routes/card')
-const addRoutes = require('./routes/add')
-const coursesRoutes = require('./routes/courses')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/card', cardRoutes)
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 async function start() {
   try {
-    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
+    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`;
     await mongoose.connect(url, {
       useNewUrlParser: true,
-      useFindAndModify: false
-    })
+      useFindAndModify: false,
+    });
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
-start()
-
-
+start();
