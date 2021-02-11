8baf12c97814cdf6073265202d9d1681291b925a
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-11 23:36:10 +0200

    connect mongo to cloud atlas, create async function start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
@@ -27,6 +28,15 @@ app.use("/card", cardRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    const url =
+      "mongodb+srv://dennypearce:aFJaYKkU7xtIDlpl@cluster0.odpci.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    await mongoose.connect(url, { useNewUrlParser: true });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
